0be90480fae9ac458e482ce8ff9108d3083f5a51
Aaron Robb <airrobb@gmail.com>
2016-02-20 20:02:38 +1300

    better tests for grid

--- test/components/Grid_spec.jsx
+++ test/components/Grid_spec.jsx
@@ -9,10 +9,17 @@ import { mount, render, shallow } from 'enzyme'
 import { Grid } from '../../src/components/Grid.jsx'
 
 describe('<Grid/>', () => {
-  it('rendes with correct props', () => {
+  it('renders with correct props', () => {
     const wrapper = shallow(<Grid grid={new Array(900).fill('')} snake={[0, 1]} food={1}/>)
 
     expect(wrapper).to.be.ok
   })
-  it('renders the correct amount of snake cells')
+  it('renders the correct amount of snake cells', () => {
+    const wrapper = mount(<Grid grid={new Array(900).fill('')} snake={[0, 1]} food={1}/>)
+    expect(wrapper.find('.snake').length).to.equal(2)
+  })
+  it('renders 900 cells', () => {
+    const wrapper = shallow(<Grid grid={new Array(900).fill('')} snake={[0, 1]} food={1}/>)
+    expect(wrapper.find('Cell').length).to.equal(900)
+  })
 })
